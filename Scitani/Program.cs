b5a8f9f1782846f90e962eb0403422b006f2d003
Sekera Alexandr <sekera.alexandr.2017@skola.ssps.cz>
2021-04-06 20:19:21 +0200

    Přidejte soubory projektu.

---
+++ Scitani/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scitani
+{
+    class Program
+    {
+        static void Main()
+        {
+            Pocitani Rada = new Pocitani(0, 0);
+            Console.WriteLine(Rada.ForCycle());
+            Console.WriteLine(Rada.WhileCycle());
+            Console.WriteLine(Rada.DoWhileCycle());
+            Console.WriteLine(Rada.RekurzeCycle());
+        }
+    }
+    class Pocitani 
+    {
+        public int X { get; set; }
+        public int N { get; set; }
+        int sum = 1;
+        public Pocitani(int x, int n) 
+        {
+            X = x;
+            N = n;
+        }
+        public int ForCycle() 
+        {
+            return sum;
+        }
+        public int WhileCycle()
+        {
+            return sum;
+        }
+        public int DoWhileCycle()
+        {
+            return sum;
+        }
+        public int RekurzeCycle()
+        {
+            return sum;
+        }
+    }
+}
